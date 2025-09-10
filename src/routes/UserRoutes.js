25ec2fd216380920d066dcfe0d09373503b91899
calebite15 <calebbite15@gmail.com>
2025-09-10 14:15:44 +0100

    update

--- src/routes/UserRoutes.js
+++ src/routes/UserRoutes.js
@@ -2,7 +2,7 @@ const express = require("express");
 const { Signup, LoginUser } = require("../controllers/Usercontroller");
 const router = express.Router();
 
-router.post("/api/Signup", Signup);
-router.post(".api/Login", LoginUser);
+router.post("/signup", Signup);
+router.post("/Login", LoginUser);
 
 module.exports = router;
