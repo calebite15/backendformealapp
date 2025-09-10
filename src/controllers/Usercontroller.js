f8a83e438135363dabc51587669d073832492ba6
calebite15 <calebbite15@gmail.com>
2025-09-10 14:41:49 +0100

    update

--- src/controllers/Usercontroller.js
+++ src/controllers/Usercontroller.js
@@ -1,17 +1,49 @@
 const Usermodel = require("../models/Usermodel");
 const bcrypt = require("bcryptjs");
+// const Signup = async (req, res) => {
+//   const { firstname, lastName, phonenumber, Email, password, favouritemeal } =
+//     req.body;
+//   try {
+//     ///check is task exists in data base
+//     const UserExist = await Usermodel.findOne({ Email });
+//     if (UserExist) {
+//       res.status(405).json({
+//         message: "USER already exists",
+//       });
+//     }
+//     ///to create new task
+//     const createNewUser = await Usermodel.create({
+//       firstname,
+//       lastName,
+//       phonenumber,
+//       Email,
+//       password,
+//       favouritemeal,
+//     });
+//     const taskResult = await createNewUser.save();
+//     res.status(200).json({
+//       firstname: taskResult.firstname,
+//       lastName: taskResult.lastName,
+//       Email: taskResult.Email,
+//       phonenumber: taskResult.phonenumber,
+//       favouritemeal: taskResult.favouritemeal,
+//         password: taskResult.password,
+//     });
+//   } catch (error) {
+//     res.status(404).json({ message: "successful" });
+//   }
+// };
 const Signup = async (req, res) => {
-  const { firstname, lastName, phonenumber, Email, password, favouritemeal } =
-    req.body;
+  const { firstname, lastName, phonenumber, Email, password, favouritemeal } = req.body;
+
   try {
-    ///check is task exists in data base
+    // Check if user exists
     const UserExist = await Usermodel.findOne({ Email });
     if (UserExist) {
-      res.status(405).json({
-        message: "USER already exists",
-      });
+      return res.status(400).json({ message: "User already exists" });
     }
-    ///to create new task
+
+    // Create new user
     const createNewUser = await Usermodel.create({
       firstname,
       lastName,
@@ -20,17 +52,13 @@ const Signup = async (req, res) => {
       password,
       favouritemeal,
     });
-    const taskResult = await createNewUser.save();
-    res.status(200).json({
-      firstname: taskResult.firstname,
-      lastName: taskResult.lastName,
-      Email: taskResult.Email,
-      phonenumber: taskResult.phonenumber,
-      favouritemeal: taskResult.favouritemeal,
-        password: taskResult.password,
+
+    res.status(201).json({
+      message: "User registered successfully",
+      user: createNewUser,
     });
   } catch (error) {
-    res.status(404).json({ message: "failed to fecth data" });
+    res.status(500).json({ message: "Failed to create user", error: error.message });
   }
 };
 
