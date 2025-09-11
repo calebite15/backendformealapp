11c4588052b31d0be10857bec1f87423750ff9f6
calebite15 <calebbite15@gmail.com>
2025-09-11 10:00:22 +0100

    cors added

--- index.js
+++ index.js
@@ -5,9 +5,11 @@ const dotenv = require("dotenv").config();
 const connectDB = require("./src/config/db");
 const recipeRoutes = require("./src/routes/recipeRoutes");
 const UserRoutes = require("./src/routes/UserRoutes");
-
+const cors=require("cors");
 connectDB();
+
 const app = express();
+app.use(cors({ origin: "*", credentials: true }));
 app.use(bodyParser.json());
 app.use("/api/Recipe", recipeRoutes);
 app.use("/api/User", UserRoutes);
